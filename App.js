d979a6049b4d6ffb3702f8d607e6d796d5fc21f8
Joel050505 <Joel.kilic@chasacademy.se>
2025-06-05 11:38:51 +0200

    Co-authored-by: Daniel Tilleria <daniel.tilleria@hotmail.com>

--- App.js
+++ App.js
@@ -1,20 +1,38 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, ImageBackground } from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ImageBackground
+      source={require("./assets/9999520.jpg")}
+      style={styles.background}
+      resizeMode="cover"
+    >
+      <View style={styles.container}>
+        <Text style={styles.text}>Welcome to Black Jack bonus fiesta</Text>
+        <StatusBar style="auto" />
+      </View>
+    </ImageBackground>
   );
 }
 
 const styles = StyleSheet.create({
+  background: {
+    flex: 1,
+  },
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
+    // Removed backgroundColor: "#fff"
+  },
+  text: {
+    color: "white",
+    fontSize: 40,
+    textAlign: "center",
+    fontWeight: "bold",
+    padding: 20,
+    position: "absolute",
+    top: "14%",
   },
 });
